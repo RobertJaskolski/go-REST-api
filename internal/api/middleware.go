25127a377fbc67ab127511136c5564142201c7df
Robert Jasklólski <rjaskolski@i2asolutions.com>
2024-03-10 21:22:22 +0100

    Configured base go CRUD project
    + Configured echo router
    + Implemented config functionality (.env files and flags)
    + Implemented custom validator for echo framework (go-playground/validator)
    + Implemented ability to connection to postgres database

---
+++ internal/api/middleware.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"reflect"
+	"strings"
+)
+
+// VALIDATOR
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return nil
+}
+
+func (s *Server) SetupValidator() {
+	validate := validator.New()
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+
+		return name
+	})
+	s.router.Validator = &CustomValidator{validator: validator.New()}
+}
+
+// NEXT ...
